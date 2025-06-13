447811b0fe2b70a2d46e38f1e375be9314fccd1e
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-06-13 15:02:31 +0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Fazliddin Abdazimov on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
