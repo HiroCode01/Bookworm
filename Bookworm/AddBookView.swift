ed22a36f8047b899d99e6a85c57b422ba68ffd1a
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-06-18 18:37:33 +0500

    Add: AddBookView page

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,70 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Fazliddin Abdazimov on 16/06/25.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title: String = ""
+    @State private var author: String = ""
+    @State private var rating: Int = 3
+    @State private var genre: String = "Fantasy"
+    @State private var review: String = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Name of the book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) { genre in
+                            Text(genre)
+                        }
+                    }
+                }
+                
+                Section("Write a review") {
+                    TextEditor(text: $review)
+                    
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                }
+                
+                Section {
+                    Button {
+                        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        modelContext.insert(newBook)
+                        dismiss()
+                    }label: {
+                        Text("Save")
+                    }
+                }
+            }
+            .navigationTitle("Add a book")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Close", systemImage: "xmark") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddBookView()
+}
--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -5,15 +5,29 @@
 //  Created by HiRO on 13/06/25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @Query var books: [Book]
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Text("Count: \(books.count)")
+                .navigationTitle("BookWorm")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add Book", systemImage: "plus") {
+                            showingAddScreen.toggle()
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
         }
         .padding()
     }
